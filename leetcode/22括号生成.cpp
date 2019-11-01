90bba8cf389d04f5731c63427b9bd9cfbd4bb0e3
hahgiraffe <646268684@qq.com>
2019-11-01 21:08:12 +0800

    update 22 26

---
+++ leetcode/22括号生成.cpp
@@ -0,0 +1,83 @@
+/*
+ * @Author: haha_giraffe
+ * @Date: 2019-11-01 20:04:33
+ * @Description: 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ * 比如输入n=3
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+ */
+
+/*
+这题目的思路就是dfs递归，第一种方法差点超时，难点在于想左右括号和递归返回的条件
+ */
+class Solution {
+public:
+//     bool check(string tmp){
+//         stack<char> sta;
+//         for(int i=0;i<tmp.size();++i){
+//             if(tmp[i]=='('){
+//                 sta.push(tmp[i]);
+//             }
+//             else{
+//                 if(sta.empty()){
+//                     return false;
+//                 }
+//                 char t = sta.top();
+//                 sta.pop();
+//             }
+//         }
+//         if(!sta.empty()){
+//             return false;
+//         }
+//         return true;
+//     }
+//     void dfs(int n ,int leftnow,int rightnow, string tmp,vector<string>& res){
+//         if(leftnow<0 || rightnow<0){
+//             return;
+//         }
+//         if(tmp.size() == n*2 && !leftnow && !rightnow){
+//             if(check(tmp)) res.push_back(tmp);
+//             return;
+//         }
+//         if(leftnow){
+//             dfs(n,leftnow-1,rightnow,tmp+'(',res);
+//         }
+//         if(rightnow){
+//             dfs(n,leftnow,rightnow-1,tmp+')',res);
+//         }
+        
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         string tmp;
+//         vector<string> res;
+//         if(!n){
+//             return res;
+//         }
+//         dfs(n,n,n,tmp,res);
+//         return res;
+//     }
+    void dfs(string tmp,vector<string>& res,int l,int r,int m){
+        if(tmp.size() == m*2){
+            res.push_back(tmp);
+            return;
+        }
+        if(l<m){
+            dfs(tmp+'(',res,l+1,r,m);
+        }
+        if(r<l){
+            dfs(tmp+')',res,l,r+1,m);
+        }
+        return;
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string tmp;
+        dfs(tmp,res,0,0,n);
+        return res;
+    }
+};
---
+++ leetcode/26删除排序数组中的重复项.cpp
@@ -0,0 +1,30 @@
+/*
+ * @Author: haha_giraffe
+ * @Date: 2019-11-01 21:05:35
+ * @Description: 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+                不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ */
+
+/* 这题目一开始没有思路，但是看到排序数组应该想到双指针的 */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        //不能用额外空间，只能原地
+        if(nums.empty()){
+            return 0;
+        }
+        int slow = 0;
+        int fast = 0;
+        while(fast < nums.size()){
+            if(nums[slow]==nums[fast]){
+                fast++;
+            }
+            else{
+                nums[slow+1]=nums[fast];
+                fast++;
+                slow++;
+            }
+        }
+        return slow+1;
+    }
+};
